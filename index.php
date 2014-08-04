f379b0e7d3ef7ce868ab5992480740c1441528fb
dandaranavarro <dandara.navarro@ccc.ufcg.edu.br>
2014-08-04 17:20:13 -0300

    adicionado index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml"> 
+<head> <meta charset="utf-8" /> 
+
+<title>Mr. Data Analyst</title> 
+
+	<link id = "estilo" rel="stylesheet" type="text/css" href="css/paginaInicial.css" />
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="javaScript/paginaInicial.js"></script> 
+
+</head>
+<body>
+	<img src = "img/investigador.jpg" width="160" height="180" class = "investigador">
+	<font id = 'nomeJogo'>Mr. Data Analyst </font>
+	<!--<div id = 'menu'>
+		<input type = "button" value = "Jogar" />
+		
+	</div>--->
+</body>
+</html>
